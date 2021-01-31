e82b1e54594676e6a518534d1afdf73e3c83359e
nicohnavarro <nicohnavarro@gmail.com>
2021-01-31 15:23:23 -0300

    :sparkles: add Cart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
-import Item from './item/Item';
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Grid from "@material-ui/core/Grid";
 import Badge from "@material-ui/core/Badge";
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
 //Styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
+import { AddShoppingCartOutlined } from "@material-ui/icons";
 
 //Types
 export type CartItemType= {
@@ -33,16 +35,26 @@ const App = () => {
     );
     console.log(data);
   
-  const getTotalItems = () => null
+  const getTotalItems = (items:CartItemType[]) => 
+    items.reduce((ack:number,item)=> ack + item.amount, 0)
   const handleAddToCart = (clickedItem: CartItemType) => null
-  const nandleRemoveFromCart = () => null
+  const handleRemoveFromCart = () => null
 
   if(isLoading) return <LinearProgress/>;
   if(error) return <div>Something went wrong</div>;
 
   return (
     <Wrapper>
-      
+      <Drawer anchor='right' open={cartOpen} onClose={()=> setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}/>
+      </Drawer>
+      <StyledButton onClick={()=> setCartOpen(true)}/>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartOutlined/>
+        </Badge>
       <Grid container spacing={3}>
       {data?.map(item =>(
         <Grid item key={item.id} xs={12} sm={4}>
