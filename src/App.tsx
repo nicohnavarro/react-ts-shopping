8de82380ceee0cbe9105a7d276675191208932db
nicohnavarro <nicohnavarro@gmail.com>
2021-01-30 23:45:24 -0300

    :fire: add Item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
+import Item from './item/Item';
+import Grid from "@material-ui/core/Grid";
+import Badge from "@material-ui/core/Badge";
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import Grid from "@material-ui/core/Grid";
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
-import Badge from "@material-ui/core/Badge";
 //Styles
 import { Wrapper } from "./App.styles";
+
 //Types
-export type CarItemType = {
+export type CartItemType= {
   id: number;
   category: string;
   description: string;
@@ -19,15 +21,22 @@ export type CarItemType = {
   amount: number;
 };
 
-const getProducts = async (): Promise<CarItemType[]> =>
+const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  const { data, isLoading, error } = useQuery<CarItemType[]>(
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
     getProducts
     );
     console.log(data);
+  
+  const getTotalItems = () => null
+  const handleAddToCart = (clickedItem: CartItemType) => null
+  const nandleRemoveFromCart = () => null
+
+  if(isLoading) return <LinearProgress/>;
+  if(error) return <div>Something went wrong</div>;
 
   return <div className="App">Start</div>;
 };
