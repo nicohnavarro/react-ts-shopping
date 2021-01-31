e2856a70c34c2cb387a244d806f7818e798d6cc6
nicohnavarro <nicohnavarro@gmail.com>
2021-01-31 15:24:38 -0300

    :label: add CarItem component

---
+++ src/CartItem/CarItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+// Types
+import { CartItemType } from '../App';
+// Styles
+import { Wrapper } from './CarItem.styles';
+
+
+const CartItem: React.FC = () => (
+  <Wrapper>
+  </Wrapper>
+)
+
+export default CartItem;
